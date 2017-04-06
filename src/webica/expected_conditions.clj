ac34011b453472e07225c01c088638d81c5c0093
Burin Choomnuan <agilecreativity@gmail.com>
2017-04-05 22:46:32 -0400

    Add support for ExpectedConditions

---
+++ src/webica/expected_conditions.clj
@@ -0,0 +1,9 @@
+(ns webica.expected-conditions
+  "Clojure binding for Selenium class ExpectedConditions"
+  (:refer-clojure :exclude [not or and])
+  (:require [clojure.core :as clj]
+            [webica.core :as w])
+  (:import [org.openqa.selenium.support.ui
+            ExpectedConditions]))
+
+(w/intern-java ExpectedConditions *ns*)

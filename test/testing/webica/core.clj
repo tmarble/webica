6626f077477f14e9fa9fe559e95f44f36032b5ed
Burin Choomnuan <agilecreativity@gmail.com>
2017-07-31 18:32:56 -0400

    Add basic tests for new functionality

--- test/testing/webica/core.clj
+++ test/testing/webica/core.clj
@@ -14,7 +14,8 @@
             [webica.remote-web-driver :as browser]
             [webica.web-driver :as driver]
             [webica.web-driver-wait :as wait]
-            [webica.web-element :as element]))
+            [webica.web-element :as element]
+            [webica.expected-conditions :as ec]))
 
 ;; inspired by
 ;; http://docs.seleniumhq.org/docs/03_webdriver.jsp#introducing-the-selenium-webdriver-api-by-example"
@@ -26,23 +27,52 @@
     (element/send-keys q "Cheese!")
     (element/submit q)
     (wait/until (wait/instance 10)
-      (wait/condition
-        (fn [driver]
-          (string/starts-with?
-            (string/lower-case (driver/get-title driver))
-            "cheese!"))))
+                (wait/condition
+                  (fn [driver]
+                    (string/starts-with?
+                      (string/lower-case (driver/get-title driver))
+                      "cheese!"))))
     (w/sleep 2)
     (browser/get-title)))
 
 (deftest testing-webica-core
   (testing "testing-webica-core"
+    #_
     (is (= expected-title
-           (let [_ (firefox/start-firefox)
+           (let [_     (firefox/start-firefox)
                  title (cheese)]
              (browser/quit)
              title)))
     (is (= expected-title
-           (let [_ (chrome/start-chrome)
+           (let [_     (chrome/start-chrome)
                  title (cheese)]
              (browser/quit)
              title)))))
+
+(def search-term "Clojure Conj 2017")
+
+;; Note: the actual url for the above search term is like:
+;; "https://www.google.com/search?site=&source=hp&q=Clojure+Conj+2017&oq=&gs_l="
+(def partial-url (apply str (interpose "+" (clojure.string/split search-term #"\s+"))))
+
+(defn lmgtfy [search]
+  (browser/get "http://www.google.com")
+  (let [wdriver (wait/init-web-driver-wait (driver/get-instance))
+        q       (browser/find-element (by/name "q"))]
+    (element/send-keys q search)
+    (element/submit q)
+    (wait/until wdriver (ec/title-contains search))
+    (driver/get-current-url)))
+
+(deftest testing-webica-core-with-expected-conditions
+  (testing "testing-webica-core-with-expected-conditions"
+    (let [_          (firefox/start-firefox)
+          actual-url (lmgtfy search-term)]
+      (browser/quit)
+      (is (string/includes? actual-url partial-url)))
+
+    (let [chrome-binary (chrome/get-chromedriver-binary "/usr/lib/chromium/chromedriver")
+          _             (chrome/start-chrome chrome-binary "headless") ;; Note: needed chromedriver version 2.30.x+ which support headless
+          actual-url    (lmgtfy search-term)]
+      (browser/quit)
+      (is (string/includes? actual-url partial-url)))))
